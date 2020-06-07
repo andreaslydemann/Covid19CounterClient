72954ef0ce7dc03cb422c3133db10cbdab9eb10f
Andreas Lüdemann <andreaslydemann@gmail.com>
2020-06-07 22:35:13 +0200

    Initial commit

---
+++ Sources/App/Country.swift
@@ -0,0 +1,14 @@
+//
+//  Country.swift
+//  App
+//
+//  Created by Andreas Lüdemann on 07/06/2020.
+//
+
+import Foundation
+import Vapor
+
+struct Country: Content {
+    var id: Int
+    var name: String
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,7 +5,11 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-    router.get("hello") { req in
-        return "Hello, world!"
+    router.get { req -> Future<View> in
+        let denmark = Country(id: 45, name: "Denmark")
+        let sweden = Country(id: 46, name: "Sweden")
+        let norway = Country(id: 47, name: "Norway")
+
+        return try req.view().render("country-selector", ["countries": [denmark, sweden, norway]])
     }
 }
